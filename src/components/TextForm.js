780ca402f7b9310450c217f3a06f1bef3aa4e9dc
hinal <khalasihinal1998@gmail.com>
2023-03-19 22:23:40 +0530

    More Features (Button)

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// background: #706464;
 
 export default function TextForm(props) {
     const [text, setText] = useState("Enter Text Here...")
@@ -11,6 +12,38 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     } 
+
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+   
+    const handleCapClick = () => {
+        const words = text.split(" ");
+        let newText = '';
+        words.forEach(element => {
+            const firstChar = element.charAt(0).toUpperCase() + element.slice(1).toLowerCase() + " ";
+            newText = newText + firstChar;
+        });
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        document.getElementById('textForm').value = "";
+    }
+
+    const handleCopyClick = () => {
+        const copyText = document.getElementById("textForm");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); 
+        navigator.clipboard.writeText(copyText.value);
+    }
+
+    const handleRemoveExtraSpaceClick = () => {
+        const newStr = text.split(/[ ]+/);
+        setText(newStr.join(" "));
+    }
     return (
         <>
             <div className='container mt-5'>
@@ -19,8 +52,18 @@ export default function TextForm(props) {
                 </div>
 
                 <div className="col-auto">
-                    <button type="submit" className="btn btn-primary mb-3" onClick={handleUpClick}>Upper Case</button>
-                </div>
+                    <button type="button" className="btn btn-primary m-1" onClick={handleUpClick}>Upper Case</button>
+                    <button type="button" className="btn btn-primary m-1" onClick={handleLowClick}>Lower Case</button>
+                    <button type="button" className="btn btn-primary m-1" onClick={handleCapClick}>Capitalized Case</button>
+                    <button type="button" className="btn btn-primary m-1" onClick={handleClearClick} >Clear</button>
+                    <button type="button" className="btn btn-primary m-1" onClick={handleCopyClick}>Copy</button>
+                    <button type="button" className="btn btn-primary m-1" onClick={handleRemoveExtraSpaceClick}>Remove Extra Spaces</button>
+                </div>       
+
+                <div className="col mt-2">
+                    <h5>Total Character : {text.length}</h5>
+                    <h5>Total Words : {text.split(" ").length}</h5>
+                </div>      
             </div>
         </>
     )
